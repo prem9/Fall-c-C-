173b3ba4a8c314c277e682bdf9005f39e7b7d337
Prem Agarwal <prem.m.agarwal@gmail.com>
2018-10-12 20:25:37 -0700

    Add files via upload

---
+++ Week5/calculate bill/calculateBill.c
@@ -0,0 +1,33 @@
+//calculateBill.c
+//Calculate total amount with x
+//Prem Agarwal
+//10/8/18
+#include <stdio.h>
+#include <math.h>
+
+float calculateBill(int hamburger, int milkshake, int hotdog)
+{
+    float result_multiply_tax, total_result;
+
+
+    result_multiply_tax = (3 *hamburger +  3*milkshake + 2*hotdog) * 0.10;
+
+    total_result = result_multiply_tax + (3*hamburger + 3*milkshake + 2*hotdog);
+    //printf("result multiplied by %.2f\n", result_multiply_tax);
+    //printf("total result is %.2f\n", total_result);
+    return total_result;
+}
+int main(void)
+{
+    int hamburger, hotdog, milkshake;
+    printf("Your order multiplied with tax of 10 percent comes to total bill is $%.2f\n", calculateBill(3, 1, 2)  );
+    printf("Your order multiplied with tax of 10 percent comes to total bill is $%.2f\n", calculateBill(8, 8, 8)  );
+    printf("How many hamburgers would you like:  ");
+    scanf("%d", &hamburger);
+    printf("\nHow many hotdogs would you like:  ");
+    scanf("%d", &hotdog);
+    printf("HOw many milkshakes would you like:  ");
+    scanf("%d", &milkshake);
+    printf("Your order multiplied with tax of 10 percent comes to total bill is $%.2f\n", calculateBill(hamburger, milkshake, hotdog)  );
+
+}
